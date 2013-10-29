dbc9c01a001fd572f9c599d7fa1d8eab14d1b30d
qqp <root@qqp.ca>
2013-10-29 19:43:20 -0400

    Add URL shortener/title fetching plugin.

---
+++ lib/Bawt/Plugin/URL.pm
@@ -0,0 +1,87 @@
+package Bawt::Plugin::URL;
+
+use strict;
+use warnings;
+
+use AnyEvent;
+use AnyEvent::HTTP;
+use HTML::PullParser;
+use Regexp::Common qw/URI/;
+
+use Bawt::IRC;
+use Bawt::RateLimiter;
+use Bawt::Utils;
+
+my $config;
+
+sub new {
+    my ($me, $cfg) = @_;
+    my $self = {};
+    bless $self, $me;
+
+    $config = $cfg;
+
+    $config->{max_urls} //= '15:60';
+    my ($count, $seconds) = split /:/, $config->{max_urls};
+    my $rate = Bawt::RateLimiter->new(actions => $count, time => $seconds);
+
+    $Bawt::irc->reg_cb(
+        irc_privmsg => sub {
+            my ($self, $msg) = @_;
+
+            print "Okay, so at least I landed here\n";
+#            return if ($heap->{'ignore'}($msg->{'prefix'}));       FIXME: Re-add ignore feature.
+
+            my $what = $msg->{params}[1];
+            while ($what =~ m/\b($RE{URI}{HTTP}{-scheme => 'https?'})/gi) {
+                last if (!$rate->check());
+                my $url = $1;
+                get_http $url, 131072, sub {
+                    my ($body, $hdr) = @_;
+                    my $title;
+
+                    if ($hdr->{Type} eq 'text/html' ||
+                         $hdr->{Type} eq 'application/xhtml' ||
+                         $hdr->{Type} eq 'application/xhtml+xml') {
+
+                        my $p = HTML::PullParser->new( 
+                            doc => $body,
+                            start => 'event',
+                            end => 'event',
+                            text => 'event, dtext',
+                            report_tags => [qw(title)]
+                        );
+
+                        my $watching = 0;
+                        while (my $token = $p->get_token) {
+                            my $event = shift(@{$token});
+                            if ($event eq 'start') { $watching = 1; }
+                            elsif ($event eq 'end') { $watching = 0; }
+                            elsif ($event eq 'text' && $watching) {
+                                $title = shift(@{$token});
+                                $title =~ s/(^\s+|\s+$)//mg;
+                                $title =~ s/\s+/ /mg;
+                                last;
+                            }
+                        }
+                    }
+
+                    chomp $title;
+                    my $target = target $msg;
+                    if (length($url) >= 35) {
+                        shorten $url, sub {
+                            my $url = shift;
+                            Bawt::IRC::msg($target, ($title) ? "$title -> $url" : "$url ($hdr->{Type})", 1);
+                        };
+                    } else {
+                        Bawt::IRC::msg($target, ($title) ? "$title -> $url" : "$url ($hdr->{Type})", 1);
+                    }
+                };
+            }
+          }
+    );
+
+    return $self;
+}
+
+1;
