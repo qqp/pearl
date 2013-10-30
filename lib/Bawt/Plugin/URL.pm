206d05f01c2b09d0e84028d87a78c732014e36d1
qqp <root@qqp.ca>
2013-10-30 15:04:31 -0400

    Refactor URL plugin

--- lib/Bawt/Plugin/URL.pm
+++ lib/Bawt/Plugin/URL.pm
@@ -14,6 +14,50 @@ use Bawt::Utils;
 
 my $config;
 
+sub __get_title {
+    my ($body, $hdr, $params) = @_;
+    my $title;
+
+    my ($url, $msg) = @$params;
+
+    if ($hdr->{Type} eq 'text/html' ||
+            $hdr->{Type} eq 'application/xhtml' ||
+            $hdr->{Type} eq 'application/xhtml+xml') {
+
+        my $p = HTML::PullParser->new( 
+            doc => $body,
+            start => 'event',
+            end => 'event',
+            text => 'event, dtext',
+            report_tags => [qw(title)]
+        );
+
+        my $watching = 0;
+        while (my $token = $p->get_token) {
+            my $event = shift(@{$token});
+            if ($event eq 'start') { $watching = 1; }
+            elsif ($event eq 'end') { $watching = 0; }
+            elsif ($event eq 'text' && $watching) {
+                $title = shift(@{$token});
+                $title =~ s/(^\s+|\s+$)//mg;
+                $title =~ s/\s+/ /mg;
+                last;
+            }
+        }
+    }
+
+    chomp $title;
+    my $target = target $msg;
+    if (length($url) >= 35) {
+        shorten $url, sub {
+            my $url = shift;
+            Bawt::IRC::msg($target, ($title) ? "$title -> $url" : "$url ($hdr->{Type})", 1);
+        };
+    } else {
+        Bawt::IRC::msg($target, ($title) ? "$title -> $url" : "$url ($hdr->{Type})", 1);
+    }
+}
+
 sub new {
     my ($me, $cfg) = @_;
     my $self = {};
@@ -35,47 +79,7 @@ sub new {
             while ($what =~ m/\b($RE{URI}{HTTP}{-scheme => 'https?'})/gi) {
                 last if (!$rate->check());
                 my $url = $1;
-                get_http $url, 131072, sub {
-                    my ($body, $hdr) = @_;
-                    my $title;
-
-                    if ($hdr->{Type} eq 'text/html' ||
-                         $hdr->{Type} eq 'application/xhtml' ||
-                         $hdr->{Type} eq 'application/xhtml+xml') {
-
-                        my $p = HTML::PullParser->new( 
-                            doc => $body,
-                            start => 'event',
-                            end => 'event',
-                            text => 'event, dtext',
-                            report_tags => [qw(title)]
-                        );
-
-                        my $watching = 0;
-                        while (my $token = $p->get_token) {
-                            my $event = shift(@{$token});
-                            if ($event eq 'start') { $watching = 1; }
-                            elsif ($event eq 'end') { $watching = 0; }
-                            elsif ($event eq 'text' && $watching) {
-                                $title = shift(@{$token});
-                                $title =~ s/(^\s+|\s+$)//mg;
-                                $title =~ s/\s+/ /mg;
-                                last;
-                            }
-                        }
-                    }
-
-                    chomp $title;
-                    my $target = target $msg;
-                    if (length($url) >= 35) {
-                        shorten $url, sub {
-                            my $url = shift;
-                            Bawt::IRC::msg($target, ($title) ? "$title -> $url" : "$url ($hdr->{Type})", 1);
-                        };
-                    } else {
-                        Bawt::IRC::msg($target, ($title) ? "$title -> $url" : "$url ($hdr->{Type})", 1);
-                    }
-                };
+                get_http $url, 131072, \&__get_title, [ $url, $msg ];
             }
           }
     );
